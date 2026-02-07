2fa6d54431310fb245ba423931edbccf44b4e476
Apolloat2022 <revanaglobal@gmail.com>
2026-02-07 16:55:58 -0600

    fix: remove legacy root files to unblock Vercel build

--- start-8081.ps1
+++
@@ -1,7 +0,0 @@
-﻿$listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add("http://localhost:8081/")
-$listener.Start()
-Write-Host "Server started on http://localhost:8081" -ForegroundColor Green
-Write-Host "Press Enter to stop" -ForegroundColor Yellow
-Read-Host
-$listener.Stop()
